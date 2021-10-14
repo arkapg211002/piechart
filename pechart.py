81aa7ff874c959b9fe52c4fa7a6013abdab9bf4e
Arkapratim Ghosh <arkapratimghosh2002@gmail.com>
2021-10-14 19:22:22 +0530

    first commit

---
+++ pechart.py
@@ -0,0 +1,12 @@
+'''import matplotlib.pyplot as p
+import numpy as n
+d=n.array([50,20,10,15,5])
+l=['python','javascript','cpp','java','php']
+p.pie(d,labels=l,shadow=True)
+p.show()'''
+import matplotlib.pyplot as p
+import numpy as n
+d=n.array([46,78,96,34])
+l=["hello","jio","alu","yoyo"]
+p.pie(d,labels=l,shadow=True)
+p.show()
